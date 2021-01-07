24637bf6234fa75a71bea957c49243a08a2f41a5
Akash <akash.w.shah@gmail.com>
2021-01-06 21:36:23 -0500

    Draft of objective hex generation system

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Prefabs & scene objects
+    // Prefabs & scene objects -------------------------------
     public GameObject hexPrefab;
     public GameObject objHexPrefab;
     public GameObject whitePiecePrefab;
     public GameObject blackPiecePrefab;
     public GameObject cam;
-    
+
+    // Game variables for tweaking ---------------------------
+    // Number of objective hexes for each player
+    public int objHexNum;
     // Hexes between each player's side
     public int rows;
     // The way/distance hexes are tiled from left to right
@@ -21,7 +25,6 @@ public class Board : MonoBehaviour
     public Vector3 columnZSpace;
     // The way/distance hexes are tiled from top to bottom
     public Vector3 columnXSpace;
-
     private Dictionary<string, GameObject>[,] hexDex;
     private bool clickedLastFrame = false;
     // Start is called before the first frame update
@@ -30,17 +33,54 @@ public class Board : MonoBehaviour
         // Set up board --------------------------------------
         // Initialize hexDex as 2D array with size of rows and columns specified
         hexDex = new Dictionary<string, GameObject>[rows, columns];
+
+        // Generate random objective hex spots
+        System.Random random = new System.Random();
+        // Makes an array that has whether or not an objective hex needs to be generated in a coordinate, then makes all values false
+        bool[,] objHexes = new bool[rows, columns];
+        for (int i = 0; i < rows * columns; i++) { objHexes[i % rows, i / rows] = false; }
+        // Initializes variables that will be used for making sure no hexes get generated in the middle row
+        // halfBoard = the number of rows that make up half the board, minus the middle row 
+        int halfBoard = rows / 2;
+        // Start loop for generation, once for each player
+        for (int i = 0; i < 2; i++)
+        {
+            // Generates the needed number of objective hexes on each side
+            for (int l = 0; l < objHexNum; l++)
+            {
+                // Chooses the rows that belong to each player
+                // If the position is already occupied by a previously generated objective hex, it will go through the do while loop again
+                int xPos, zPos;
+                do
+                {
+                    xPos = random.Next(0, rows);
+                    if (i == 0) 
+                    {
+                        zPos = random.Next(0, halfBoard);
+                    } 
+                    else
+                    {
+                        zPos = random.Next(halfBoard + 1, columns);
+                    }
+                    objHexes[xPos, zPos] = true;
+                } 
+                while (objHexes[xPos, zPos]);
+            } 
+        }
         // lastPosition = the last place we spawned in a hex, we'll then add some vectors to it to get our new position and spawn a new hex there
         Vector3 lastPosition = new Vector3(0f, 0f, 0f);
         // Whether or not the first hex in the last row generated was offsetted to the right
         bool lastWentRight = true;
+        // Loop through each row and hex in each row ---------
         for (int i = 0; i < hexDex.GetLength(0); i++)
         {
             for (int hexX = 0; hexX < hexDex.GetLength(1); hexX++)
             {
                 // Spawn in hex and put that in the array
                 hexDex[i, hexX] = new Dictionary<string, GameObject>();
-                hexDex[i, hexX].Add("hex", Instantiate(hexPrefab, lastPosition, Quaternion.Euler(0f, 30f, 0f)));
+                GameObject hexToPlace;
+                if (objHexes[i, hexX]) { hexToPlace = objHexPrefab; } else { hexToPlace = hexPrefab; }
+                hexDex[i, hexX].Add("hex", Instantiate(hexToPlace, lastPosition, Quaternion.Euler(0f, 30f, 0f)));
                 // Offets next hex position (for next time through loop)
                 lastPosition += rowSpace;
             }
