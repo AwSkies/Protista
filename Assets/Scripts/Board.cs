da1a1435b7d9e4b8b33bf51612fed745d889992d
Akash <akash.w.shah@gmail.com>
2021-01-11 14:56:17 -0500

    Generate pieces on hexes

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -25,6 +25,11 @@ public class Board : MonoBehaviour
     public Vector3 columnZSpace;
     // The way/distance hexes are tiled from top to bottom
     public Vector3 columnXSpace;
+    // Number of pieces for each player
+    public int pieceNum;
+    // Vertical offset of each piece
+    public Vector3 pieceVertical;
+
     private Dictionary<string, GameObject>[,] hexDex;
     private bool clickedLastFrame = false;
     // Start is called before the first frame update
@@ -33,14 +38,15 @@ public class Board : MonoBehaviour
         // Set up board --------------------------------------
         // Initialize hexDex as 2D array with size of rows and columns specified
         hexDex = new Dictionary<string, GameObject>[rows, columns];
-
-        // Generate random objective hex spots
+        // halfBoard = the number of rows that make up half the board, minus the middle row 
+        int halfBoard = rows / 2;
+        // Initialize random
         System.Random random = new System.Random();
+
+        // Generate objective hexe arrangement ---------------
         // Makes an array that has whether or not an objective hex needs to be generated in a coordinate, then makes all values false
         bool[,] objHexes = new bool[rows, columns];
         for (int i = 0; i < rows * columns; i++) { objHexes[i % rows, i / rows] = false; }
-        // halfBoard = the number of rows that make up half the board, minus the middle row 
-        int halfBoard = rows / 2;
         // Generates the needed number of objective hexes on each side
         for (int i = 0; i < objHexNum; i++)
         {
@@ -49,28 +55,80 @@ public class Board : MonoBehaviour
             int xPos, zPos;
             do
             {
+                // Choose position on one half of the board
                 xPos = random.Next(0, columns);
                 zPos = random.Next(0, halfBoard);
+                // Add objective hex at chosen position
                 objHexes[zPos, xPos] = true;
+                // Mirror objective hexes across board
                 zPos = (rows - 1) - zPos;
+                // Add objective hex at mirrored position
                 objHexes[zPos, xPos] = true;
             } 
             while (!objHexes[zPos, xPos]);
         }
+
+        // Generate piece arrangement ------------------------
+        // Makes an array that has whether or not a piece needs to be generated in a coordinate, then makes all values false
+        bool[,] pieces = new bool[rows, columns];
+        for (int i = 0; i < rows * columns; i++) { pieces[i % rows, i / rows] = false; }
+        // Generates the needed number of pieces on each side
+        for (int i = 0; i < pieceNum; i++)
+        {
+            // Chooses the rows that belong to each player
+            // If the position is already occupied by a previously generated piece, it will go through the do while loop again
+            int xPos, zPos;
+            do
+            {
+                // Choose position on one half of the board
+                xPos = random.Next(0, columns);
+                zPos = random.Next(0, halfBoard);
+                // Add piece at chosen position
+                pieces[zPos, xPos] = true;
+                // Mirror pieces across board
+                zPos = (rows - 1) - zPos;
+                // Add piece at mirrored position
+                pieces[zPos, xPos] = true;
+            } 
+            while (!pieces[zPos, xPos]);
+        }
+
+        // Generating gameboard ------------------------------
         // lastPosition = the last place we spawned in a hex, we'll then add some vectors to it to get our new position and spawn a new hex there
         Vector3 lastPosition = new Vector3(0f, 0f, 0f);
         // Whether or not the first hex in the last row generated was offsetted to the right
         bool lastWentRight = true;
-        // Loop through each row and hex in each row ---------
+        // Loop through each row and hex in each row
         for (int i = 0; i < hexDex.GetLength(0); i++)
         {
             for (int hexX = 0; hexX < hexDex.GetLength(1); hexX++)
             {
-                // Spawn in hex and put that in the array
+                // Create empty dictionary in position
                 hexDex[i, hexX] = new Dictionary<string, GameObject>();
+
+                // Spawn hexes -------------------------------
+                // Choose to place normal or objective hex based on earlier generation
                 GameObject hexToPlace;
                 if (objHexes[i, hexX]) { hexToPlace = objHexPrefab; } else { hexToPlace = hexPrefab; }
+                // Spawn hex and place in hexDex
                 hexDex[i, hexX].Add("hex", Instantiate(hexToPlace, lastPosition, Quaternion.Euler(0f, 30f, 0f)));
+
+                // Spawn pieces ------------------------------
+                // Choose whether or not to spawn pieces based on earlier generation
+                if (pieces[i, hexX])
+                {
+                    // Choose to place black or white piece depending on position in the board
+                    GameObject pieceToPlace;
+                    if (i < halfBoard) { pieceToPlace = whitePiecePrefab; } else { pieceToPlace = blackPiecePrefab; }
+                    // Spawn piece above hex and place in hexDex
+                    hexDex[i, hexX].Add("piece", Instantiate(pieceToPlace, lastPosition + pieceVertical, Quaternion.identity));
+                }
+                // Make sure every index in the hexDex has a piece field so we can tell which ones don't have pieces
+                else
+                {
+                    hexDex[i, hexX].Add("piece", null);
+                }
+
                 // Offets next hex position (for next time through loop)
                 lastPosition += rowSpace;
             }
