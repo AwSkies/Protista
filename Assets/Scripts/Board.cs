34a7d65c6ef8bad0bf1aab3f2e1df9e02dac319b
Akash <akash.w.shah@gmail.com>
2021-11-14 17:24:32 -0500

    No longer initialize hexDex in Board Start method The Start method in Board was called after the Start method in GameManager and would reset the hexDex to an empty array

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,21 +4,6 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Reference to Game Manager
-    public GameManager gameManager;
-
     // Index of hexes ordered by z, x position
     public GameObject[,] hexDex;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        hexDex = new GameObject[gameManager.rows, gameManager.columns];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
