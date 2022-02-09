4ed0af3cb0efb31b3f4324824df988f23d10925b
Akash <akash.w.shah@gmail.com>
2022-02-09 16:40:04 -0500

    Write summary for Hex

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>Contains data about a hex, like the its neighbors stored in an array and the piece on this hex.</summary>
 public class Hex : MonoBehaviour
 {
     public GameObject[] neighbors = new GameObject[6];
