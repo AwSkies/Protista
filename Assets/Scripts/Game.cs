4edf4911702a7b764fe5f9bb5ce2164803577780
Akash <akash.w.shah@gmail.com>
2022-07-27 14:17:39 -0400

    Create json layout scrolling window

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 /// <summary>Stores information about the type and parameters of the game and contains button functions for configuring them.</summary>
@@ -11,6 +13,17 @@ public class Game : MonoBehaviour
     // The deserialized data
     public static Layout layout;
     public static GameType gameType;
+
+    [SerializeField]
+    private GameObject noLayoutsText;
+    [SerializeField]
+    private GameObject openFolderButton;
+    [SerializeField]
+    private GameObject layoutButtonPrefab;
+    [SerializeField]
+    private Transform contentField;
+
+    private List<GameObject> layoutButtons = new List<GameObject>();
 
     /// <summary>Loads the GameBoard scene containing the actual game board.
     /// The static fields layout and gameType will be processed by the board GameManager script and be handled accordingly.</summary>
@@ -23,12 +36,35 @@ public class Game : MonoBehaviour
     public void DisplayJsonLayouts()
     {
         // Selects json files in the persistent data path
-        string[] files = (string[]) from file in Directory.GetFiles(Application.persistentDataPath) where file.EndsWith(".json") select file;
-        foreach (string path in files)
+        var files = from file in Directory.GetFiles(Application.persistentDataPath) where file.EndsWith(".json") select file;
+        if (files.Count() > 0)
         {
-            // Make a button to click on for each of the names
+            // Set the no layouts text and button inactive
+            noLayoutsText.SetActive(false);
+            openFolderButton.SetActive(false);
             
+            // Create a button for each file
+            foreach (string path in files)
+            {
+                // Make a button to click on for each of the names
+                // Instantiates a button at the correct position with no rotation and parented to the content field
+                GameObject button = Instantiate(layoutButtonPrefab, contentField);
+                // Sets the text of the button to be the name of the layout
+                button.transform.GetChild(0).GetComponent<TMP_Text>().SetText(path);
+                // Add button to list
+                layoutButtons.Add(button);
+            }
         }
+    }
+
+    /// <summary>Deletes the buttons for the json layouts</summary>
+    public void DeleteJsonLayouts()
+    {
+        foreach (GameObject button in layoutButtons)
+        {
+            Destroy(button);
+        }
+        layoutButtons = new List<GameObject>();
     }
 
     /// <summary>Sets game type to corresponding enum based on a string</summary>
@@ -49,5 +85,10 @@ public class Game : MonoBehaviour
                 gameType = GameType.PassAndPlay;
                 break;
         }
+    }
+
+    public void OpenFolder()
+    {
+        System.Diagnostics.Process.Start("explorer.exe", Application.persistentDataPath);
     }
 }
