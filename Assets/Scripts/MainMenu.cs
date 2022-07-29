3181fa250b66266015f858fe879d30e731b07d74
Akash <akash.w.shah@gmail.com>
2022-07-29 17:28:03 -0400

    Implement code for formatting layout info panel with proper information

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,10 +12,13 @@ using UnityEngine.SceneManagement;
 /// <summary>Stores information about the type and parameters of the game and contains button functions for configuring them.</summary>
 public class MainMenu : MonoBehaviour
 {
-    // The deserialized data
+    #region Static data recorded for passing to GameBoard scene
     public static Layout layout;
     public static GameType gameType;
+    #endregion
 
+    #region Main menu scene objects
+    #region Layout list
     [SerializeField]
     private GameObject noLayoutsText;
     [SerializeField]
@@ -24,23 +27,65 @@ public class MainMenu : MonoBehaviour
     private GameObject layoutButton;
     [SerializeField]
     private Transform contentField;
+    #endregion
 
+    #region Layout information window
+    #region Basic information
     [SerializeField]
     private TMP_Text title;
     [SerializeField]
     private TMP_Text author;
     [SerializeField]
     private TMP_Text description;
+    #endregion
+
+    #region Size information
     [SerializeField]
-    private TMP_Text rowText;
+    private GameObject sizeGroup;
     [SerializeField]
     private TMP_Text rowCount;
     [SerializeField]
-    private TMP_Text columnText;
-    [SerializeField]
     private TMP_Text columnCount;
+    #endregion
+
+    #region Piece information
+    [SerializeField]
+    private GameObject pieceGroup;
+    [SerializeField]
+    private GameObject pieceEachGroup;
+    [SerializeField]
+    private TMP_Text pieceEachNum;
+    [SerializeField]
+    private GameObject pieceWhiteGroup;
+    [SerializeField]
+    private TMP_Text pieceWhiteNum;
+    [SerializeField]
+    private GameObject pieceBlackGroup;
+    [SerializeField]
+    private TMP_Text pieceBlackNum;
+    #endregion
+
+    #region Objective hex information
+    [SerializeField]
+    private GameObject objectiveHexGroup;
+    [SerializeField]
+    private GameObject objectiveHexEachGroup;
+    [SerializeField]
+    private TMP_Text objectiveHexEachNum;
+    [SerializeField]
+    private GameObject objectiveHexWhiteGroup;
+    [SerializeField]
+    private TMP_Text objectiveHexWhiteNum;
+    [SerializeField]
+    private GameObject objectiveHexBlackGroup;
+    [SerializeField]
+    private TMP_Text objectiveHexBlackNum;
+    #endregion
+
     [SerializeField]
     private GameObject playButton;
+    #endregion
+    #endregion
 
     private List<GameObject> layoutButtons = new List<GameObject>();
 
@@ -89,48 +134,136 @@ public class MainMenu : MonoBehaviour
     /// <summary>Deletes the buttons for the json layouts</summary>
     public void DeleteJsonLayouts()
     {
+        // Destroy each layout in the list
         foreach (GameObject button in layoutButtons)
         {
             Destroy(button);
         }
+        // Reset button list
         layoutButtons = new List<GameObject>();
     }
 
     public void DisplayInformation(LayoutPath path)
     {
+        // Read json from file
         string json = File.ReadAllText(path.path);
+        // Whether or not the file is valid
+        bool valid = true;
+        // Record if there is an error parsing
         try
         {
             layout = JsonConvert.DeserializeObject<Layout>(json);
         }
-        catch (JsonReaderException)
+        catch (JsonException)
         {
-            layout = null;
+            valid = false;
         }
+
+        // Set title to file name
         title.SetText(Path.GetFileNameWithoutExtension(path.path));
-        if (layout != null)
+        // If the layout is valid
+        if (valid)
         {
+            // Set author, description
             author.SetText("By " + layout.Author);
             description.SetText(layout.Description);
+
+            // Set board sizes
             rowCount.SetText(layout.Rows.ToString());
             columnCount.SetText(layout.Columns.ToString());
-            author.gameObject.SetActive(true);
-            rowText.gameObject.SetActive(true);
-            rowCount.gameObject.SetActive(true);
-            columnText.gameObject.SetActive(true);
-            columnCount.gameObject.SetActive(true);
-            playButton.gameObject.SetActive(true);
+
+            #region Piece information
+            // If there are prespecified pieces
+            bool piecesSpecified = layout.Pieces != null;
+            if (piecesSpecified)
+            {
+                // Count number of black and white pieces
+                int black = 0, white = 0;
+                foreach (PieceInfo piece in layout.Pieces)
+                {
+                    // The actual number of pieces that this piece has, accounting for stacks
+                    int number = piece.Stacked + 1;
+                    if (piece.White)
+                    {
+                        white += number;
+                    }
+                    else
+                    {
+                        black += number;
+                    }
+                }
+
+                // Set white and black numbers to the number of pieces
+                pieceWhiteNum.SetText(white.ToString());
+                pieceBlackNum.SetText(black.ToString());
+            }
+            // If a not default piece num is given
+            else if (layout.PieceNum != null)
+            {
+                pieceEachNum.SetText(layout.PieceNum.ToString());
+            }
+            // If the default amount of pieces is wanted
+            else
+            {
+                pieceEachNum.SetText(15.ToString());
+            }
+            // Set correct set of information groups active
+            pieceEachGroup.SetActive(!piecesSpecified);
+            pieceWhiteGroup.SetActive(piecesSpecified);
+            pieceBlackGroup.SetActive(piecesSpecified);
+            #endregion
+
+            #region Objective hex information
+            // If there are prespecified pieces
+            bool objectiveHexesSpecified = layout.ObjectiveHexes != null;
+            if (objectiveHexesSpecified)
+            {
+                // Count number of black and white objective hexes
+                int black = 0, white = 0;
+                foreach (int[] objectiveHex in layout.ObjectiveHexes)
+                {
+                    // Counts as black or white depending on which side of the board it's on
+                    if (objectiveHex[0] < layout.Rows / 2)
+                    {
+                        white++;
+                    }
+                    else
+                    {
+                        black++;
+                    }
+                }
+
+                // Set white and black numbers to the number of objective hexes
+                objectiveHexWhiteNum.SetText(white.ToString());
+                objectiveHexBlackNum.SetText(black.ToString());
+            }
+            // If a not default objectiveHex num is given
+            else if (layout.ObjectiveHexNum != null)
+            {
+                objectiveHexEachNum.SetText(layout.ObjectiveHexNum.ToString());
+            }
+            // If the default amount of objectiveHexes is wanted
+            else
+            {
+                objectiveHexEachNum.SetText(3.ToString());
+            }
+            // Set correct set of information groups active
+            objectiveHexEachGroup.SetActive(!objectiveHexesSpecified);
+            objectiveHexWhiteGroup.SetActive(objectiveHexesSpecified);
+            objectiveHexBlackGroup.SetActive(objectiveHexesSpecified);
+            #endregion
         }
         else
         {
-            description.SetText("There is an error with this layout file. Check the formatting to make sure it is correct.");
-            author.gameObject.SetActive(false);
-            rowText.gameObject.SetActive(false);
-            rowCount.gameObject.SetActive(false);
-            columnText.gameObject.SetActive(false);
-            columnCount.gameObject.SetActive(false);
-            playButton.gameObject.SetActive(false);
+            description.SetText("There is an error with this layout file. Check the formatting and try again.");
         }
+
+        // Set everything active or inactive based on validity
+        author.gameObject.SetActive(valid);
+        playButton.gameObject.SetActive(valid);
+        sizeGroup.SetActive(valid);
+        pieceGroup.SetActive(valid);
+        objectiveHexGroup.SetActive(valid);
     }
 
     /// <summary>Sets game type to corresponding enum based on a string</summary>
