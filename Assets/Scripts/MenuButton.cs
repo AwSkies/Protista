d8617a675e7a589a0e5966848d687dcbf9cd32f7
Akash <akash.w.shah@gmail.com>
2022-08-27 01:13:52 -0400

    Make menu buttons look better

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject description;
+
+    public void OnDisable()
+    {
+        description.SetActive(false);
+    }
+}
